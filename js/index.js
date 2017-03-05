e97323f63564df87ff1770263596847a440e3326
Fabrício Jean <fabriciojsil@gmail.com>
2017-03-05 19:06:45 -0300

    almost there

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     $('.parallax').parallax();
+    
+    $(".button-collapse").sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
+
+    $("nav").find("a").click(function(e) {
+        e.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top
+        });
+    });
+
+
 });
